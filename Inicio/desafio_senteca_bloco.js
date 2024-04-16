2f6209c90d26d5342d596d6aa3e48ee8c093328d
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-16 20:08:44 -0300

    desafio_01
    primeira atividade

---
+++ Inicio/desafio_senteca_bloco.js
@@ -0,0 +1,30 @@
+/*
+// Bloco impar
+1
+3
+5
+7
+9
+
+// Bloco par
+2
+4
+6
+8
+10
+*/
+{
+    console.log("# 01");
+    console.log("# 03");
+    console.log("# 05");
+    console.log("# 07");
+    console.log("# 09");
+}
+
+{
+    console.log("# 02");
+    console.log("# 04");
+    console.log("# 06");
+    console.log("# 08");
+    console.log("# 10");
+}
