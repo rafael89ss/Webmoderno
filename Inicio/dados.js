739b72c4dc9251a9657ff1864829c983cdd240f1
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-16 20:34:35 -0300

    declarar_variavel
    Aprendendo a declarar variaveis

---
+++ Inicio/dados.js
@@ -0,0 +1,12 @@
+// Declaração de Variaveis
+
+var QTD = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto
+
+console.log("Caneta");
+console.log(QTD);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
