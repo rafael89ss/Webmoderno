96e1e924e61fe5b991ecac7275d281e8b78bc6d6
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-20 14:01:35 -0300

    desafio
    área de troca.

---
+++ Inicio/desafio_atea.js
@@ -0,0 +1,21 @@
+// Quando é uma constante, é essencialmete utilizado com caracter maiusculo como boas praticas
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+
+let raio = 10;
+//let areaCirc = math.PI * raio * raio;
+
+console.log("O valor da soma PI x raio x raio é igual a " + PI * raio * raio + "m².");
+//console.log("O calor da área é " + areaCirc + "m².");
+
+
+
+// Resposta do professor
+
+//const PI = 3.141592; // ou usar o math.PI
+//let raio = 10;
+//let areaCirc = PI * raio * raio;
+//
+//console.log("O valor da área é " + areaCirc);
---
+++ Inicio/desafio_troca.js
@@ -0,0 +1,21 @@
+let a = 7;
+let b = 94;
+
+console.log("O valor de a =" + a);
+console.log("O valor de a =" + b);
+
+// Outra forma de Troca
+//[a, b] = [b, a];
+//console.log("O valor de a =" + a);
+//console.log("O valor de a =" + b);
+
+
+// depois  da troca ... a = 94 e b = 7 
+let temp = a;
+a = b;
+b = temp;
+
+console.log("O valor de a =" + a);
+console.log("O valor de a =" + b);
+
+
