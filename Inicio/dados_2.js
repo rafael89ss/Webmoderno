24d32ec9e9036fbb98a25cea5113d9e9e7a9afa0
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-20 12:50:11 -0300

    Update dados_2.js
    continuação da aula.

--- Inicio/dados_2.js
+++ Inicio/dados_2.js
@@ -5,3 +5,11 @@ console.log(19.9 * 0.6);
 
 let precoComDesconto = preco * (1 - desconto);
 console.log(precoComDesconto);
+
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
