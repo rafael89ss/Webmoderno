6ce92854cf3277db396ca4417d82912a4528e274
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-21 12:06:31 -0300

    comentarios
    comentários

---
+++ exercicios-js/comentario.js
@@ -0,0 +1,15 @@
+// Comentario em uma única linha
+console.log("Tipo comentário 1");
+
+/*
+Comentário de
+múltiplas linhas
+*/
+console.log("Tipo comentário 2");
+
+/*
+ * Comentário de
+ * múltiplas linhas
+*/
+console.log("Tipo comentário 3");
+
