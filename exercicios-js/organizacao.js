07a3b4aa1c5d119b1be5e1ad04c317b5a4ec718a
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-21 11:38:14 -0300

    organizacao
    sentenças e bloco

---
+++ exercicios-js/organizacao.js
@@ -0,0 +1,9 @@
+// Sentenças de códigos
+console.log("Linha de código = sentença de código");
+
+// Bloco de códigos = conjuto de sentenças de códigos
+// com abertura de chave 
+{
+    console.log("Olá");
+    console.log('Mundo!');
+}
