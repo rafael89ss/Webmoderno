400b58e7fef0f39c9f9e4a9b522e7bb98a1fda0f
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-21 14:54:35 -0300

    numerosalgunscuidades

---
+++ exercicios-js/numerosAlgunsCuidadeos.js
@@ -0,0 +1,6 @@
+console.log(7 / 0);  // Atribui um valor infinito.
+console.log("10" / 2); // Mesmo o valor entre aspas sendo uma string o javascript verifica e ver pode realizar a operação.
+console.log("Show!" * 2);  //Informa que isto Não é um número "NaN"
+console.log(0.1 + 0.7);  //Não apresentará o valor real, por se tratar do tipo de operação que o javaScript suporta.
+//console.log(10.toString());  // Não consegue desta forma transformar para String.
+console.log((10).toFixed(2));  // Desta forma consegue.
