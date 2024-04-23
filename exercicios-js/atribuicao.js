6bcf40424bfe9240eb7bce8bffeac9b57229593d
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-23 19:56:31 -0300

    atribuicao

---
+++ exercicios-js/atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+let b = 3
+
+b += a  // é o mesmo que b = b + a
+console.log(b);
+
+b -= 4  // é o mesmo que b = b -4
+console.log(b);
+
+b *= 2  // é o mesmo que b = b *2
+console.log(b);
+
+b /= 2  // é o mesmo que b = b / 2
+console.log(b);
+
+b %= 2  // é o mesmo que b = b % 2  >> se o resultado de "B" for PAR a saída é 0(zero), se for IMPAR então a saída é 1
+console.log(b);
+
