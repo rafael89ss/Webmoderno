dc63aa84755bf58e880fe0569a9208f9bd7fb0c2
Rafael dos Santos Silva <rafael@SEE.PB>
2024-04-27 19:24:55 -0300

    destructurings

---
+++ exercicios-js/destructuring3.js
@@ -0,0 +1,11 @@
+/**
+ * Função para retornar um número aleatório
+ * 
+ */
+
+function rand({ min = 0, max = 1000}) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+const obj = { max: 50, min: 40}
+console.log(rand(obj));
