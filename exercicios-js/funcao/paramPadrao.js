31b6e0fe0c2085005bdde13ccc1f07fa0c8d977a
Rafael dos Santos Silva <rafael-17ss@hotmail.com>
2024-05-13 16:48:01 -0300

    parampadraII

--- exercicios-js/funcao/paramPadrao.js
+++ exercicios-js/funcao/paramPadrao.js
@@ -8,7 +8,7 @@ function soma1(a, b, c){
 
 console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))  // Nesta ultima que é zero, puxará o segundo campo "1"
 
-// Estrategia 2, 3 e 4 para gerar valor padrão
+// Estrategia 2, 3 e 4 para gerar valor padrão  - Segunda melhor Versão
 function soma2(a, b, c) {
     a = a !== undefined ? a : 1
     b = 1  in arguments ? b : 1
@@ -19,7 +19,7 @@ function soma2(a, b, c) {
 console.log(soma2(), soma2(3), soma2(1, 2, 3), soma3(0, 0, 0))
 
 
-// Valor padrão do es2015
+// Valor padrão do es2015  - Melhor Versão
 function soma3(a = 1, b = 1, c = 1){
     return a + b + c
 }
