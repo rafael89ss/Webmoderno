c4a75140c9d6964d90ce7fd2cfd76546b27a9278
Rafael dos Santos Silva <rafael@SEE.PB>
2024-05-15 19:24:54 -0300

    callback3

---
+++ exercicios-js/funcao/callback3.js
@@ -0,0 +1,9 @@
+// Exemplo de Callback no browser
+
+document.getElementsByTagName('body')[0].onclick = function(e) {
+    console.log('O evento ocorreu!')
+}
+
+/**
+ * Exemplo para executar em Browser, executa e depois clica na pag.
+ */
