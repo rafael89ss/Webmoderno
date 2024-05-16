2e2c44a3eb34e5ff4a9612279dd5fa5beeda52b4
Rafael dos Santos Silva <rafael-17ss@hotmail.com>
2024-05-16 14:04:37 -0300

    tiposDeclaração

---
+++ exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,20 @@
+console.log(soma(3, 4))
+// Function declaration
+
+function soma(x, y) {
+    return x + y
+}
+
+// Function expression                 Só funciona depois que a função é declarada
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(3, 4))
+
+// name function expression            Só funciona depois que a função é declarada 
+const mult = function mult(x, y) {
+    return x * y
+}
+console.log(mult(3, 4))
+
+
