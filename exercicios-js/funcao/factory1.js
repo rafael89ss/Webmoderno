1790d210d51a3297c347a119e4a67fc8b6d59271
Rafael dos Santos Silva <rafael@SEE.PB>
2024-05-16 20:46:13 -0300

    factoryfunction

---
+++ exercicios-js/funcao/factory1.js
@@ -0,0 +1,12 @@
+// Função factory é uma função que retorna um objeto.
+
+// Factory simples
+
+function criarPessoa(){
+    return {
+        nome: 'Ana',
+        Sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
---
+++ exercicios-js/funcao/factory2.js
@@ -0,0 +1,12 @@
+// Factory
+
+function criarProduto(nome, preco){
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto("notebook", 2199.49))
+console.log(criarProduto('iPad', 1199.49))
